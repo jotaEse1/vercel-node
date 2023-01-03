931e57a4ceb10397a7f0312c1250bccd77d8f667
Joaquin <joacosanchez20@gmail.com>
2023-01-03 15:17:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+
+//port
+const port = process.env.PORT || 8000
+
+app.get('/home', (req, res) => {
+    res.send('Hello!')
+})
+
+app.use('*', (req, res) => {
+    res.send('<h1>Page not found</h1>')
+})
+
+app.listen(port, (req, res) => {
+    console.log(`Server is running on port ${port}`)
+})
